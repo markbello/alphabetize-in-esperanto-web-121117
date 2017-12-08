1057636fcb39cf810a92bc04809d5a4dcd2e3ea3
Mark Bello <mcbello51286@gmail.com>
2017-12-08 18:07:17 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,8 @@
 def alphabetize(arr)
-  # code here
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars
+
+  arr.sort_by do |phrase|
+    phrase.chars.map { |char| alphabet.index(char)}
+  end
+end
 end
